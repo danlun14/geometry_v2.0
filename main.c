7017bae7f0f64a51a03d50274672ae96cb6ed2d3
danila <danila@localhost.localdomain>
2020-03-09 00:45:55 +0700

    add input function

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
+<<<<<<< HEAD
 #include <stdio.h>
 
 int main(){
     
 }
 
+||||||| merged common ancestors
+=======
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char all[128];
+    printf("Введите входные данные формата:\n");
+    printf("circle(x y, r) - для круга\n");
+    fgets(all, 128, stdin);
+}
+>>>>>>> add input function
