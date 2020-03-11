e17416de45b09222c6a8d18c4cf91aa930317a3c
danila <danila@localhost.localdomain>
2020-03-11 09:48:50 +0700

    create check for figure name

---
+++ functions.h
@@ -0,0 +1,1 @@
+int fcheck(char *arr);
--- main.c
+++ main.c
@@ -2,12 +2,17 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <stdlib.h>
+#include "functions.h"
 
 int main()
 {
+    int f_res = 0;
     char all[128];
     printf("Введите входные данные формата:\n");
     printf("circle(x y, r) - для круга\n");
     fgets(all, 128, stdin);
     printf("%s", all);
+    f_res = fcheck(all);
+    printf("%d\n", f_res);
 }
