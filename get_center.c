9557291c870336a13b1af99174c4e72e1c4dd34c
danila <danila@localhost.localdomain>
2020-03-11 11:10:28 +0700

    end of develop function, which takes center of circle

--- get_center.c
+++ get_center.c
@@ -1,5 +1,39 @@
+#include <stdlib.h>
+#define NUM_SYS 10
 
-
-int get_center(circle *cir, char arr)
+int get_center(circle *cir, char *arr)
 {
+    char *cur, *end;
+    int num, i = 0;
+    long int coordinate;
+    num = arr[i];
+    while (num != '(')
+    {
+        i++;
+        num = arr[i];
+    }
+    cur = arr + 1 + i;
+    coordinate = strtol(cur, &end, NUM_SYS);
+    if (cur - end != 0)
+    {
+        cir->point.x = coordinate;
+        cur = end;
+        coordinate = strtol(cur, &end, NUM_SYS);
+        if (cur - end != 0)
+        {
+            cir->point.y = coordinate;
+            cur = end;
+            num = *cur;
+            while (num == ' ')
+            {
+                cur++;
+                num = *cur;
+            }
+            if (num == ',')
+            {
+                return 1;
+            }
+        }
+    }
+    return -1;
 }
